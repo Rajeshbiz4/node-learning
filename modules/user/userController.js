8b98bfafc1488a945533e35c2ea3c51fa350b960
rajesh.pandhare@innoplexus.com <rajesh.pandhare@innoplexus.com>
2019-01-28 11:30:27 +0530

    new API added.

--- modules/user/userController.js
+++ modules/user/userController.js
@@ -3,8 +3,9 @@ var router = express.Router()
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017/';
 
-// respond with "hello world" when a GET request is made to the homepage
+// Create new user
 router.post('/create', function (req, res) {
+  console.log('--USER_CREATE--')
   console.log(req.body)
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     if (err) throw err;
@@ -25,23 +26,47 @@ router.post('/create', function (req, res) {
   });
   res.send('document inserted')
 })
-// define the about route
-router.get('/list', function (req, res) {
 
+// get All list
+router.get('/list', function (req, res) {
+  console.log('--USER_LIST--')
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.collection("customers").findOne({ name:"Rajesh Pandhare"}, function(err, result) {
+    dbo.collection("customers").find({}).toArray(function(err, result) {
       if (err) throw err;
-      console.log(result);
+      console.log("customers", result);
       if(result){
-        res.send(result)
-      } else {
-        res.send('NO data found')
-      }
+            res.send(result)
+          } else {
+            res.send('NO data found')
+          }
       db.close();
     });
   });
 })
 
+// Update one document
+router.post('/update', function (req, res) {
+  console.log('--USER_UPDATE--')
+  MongoClient.connect(url, function(err, db) {
+    var payload = {
+      "name": req.body.name
+    }
+    if (err) throw err;
+  var dbo = db.db("mydb");
+  var myquery = payload
+  var newvalues = { $set: {name: "YO Rajesh" }}
+  dbo.collection("customers").updateOne(myquery, newvalues, function(err, result) {
+    if (err) throw err;
+    console.log("1 document updated");
+    res.send("1 document updated")
+    db.close();
+  });
+  });
+})
+
+
+
+ 
 module.exports = router
