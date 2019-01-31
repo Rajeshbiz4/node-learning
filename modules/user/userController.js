03adb21a77442e28f52e7a06bb2014cee194ce43
rajesh.pandhare@innoplexus.com <rajesh.pandhare@innoplexus.com>
2019-01-31 14:42:59 +0530

    logger file added.

--- modules/user/userController.js
+++ modules/user/userController.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var router = express.Router()
 var MongoClient = require('mongodb').MongoClient;
+var myLogModule = require('../../utils/logger');
 var url = 'mongodb://127.0.0.1:27017/';
 
 // Create new user
 router.post('/create', function (req, res) {
-  console.log('--USER_CREATE--')
-  console.log(req.body)
-  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  myLogModule.info('UserController API-UserList(user/create)')
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
    var payload = {
@@ -17,28 +17,33 @@ router.post('/create', function (req, res) {
       "taluka":req.body.taluka,
       "village":req.body.village,
     }
+    myLogModule.info('payload -- ' + JSON.stringify(payload))
     var myobj = payload
     dbo.collection("customers").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("document inserted");
+      if (err) {
+        myLogModule.error('error while user creating')
+      } else {
+        myLogModule.error('error while user creating')
+        res.send('document inserted')
+      }
       db.close();
     });
   });
-  res.send('document inserted')
 })
 
 // get All list
 router.get('/list', function (req, res) {
-  console.log('--USER_LIST--')
+  myLogModule.info('UserController API-UserList(user/list)')
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
     dbo.collection("customers").find({}).toArray(function(err, result) {
       if (err) throw err;
-      console.log("customers", result);
       if(result){
+            myLogModule.info('UserController - User list fetched sucessfully')
             res.send(result)
           } else {
+            myLogModule.error('UserController - NO data found')
             res.send('NO data found')
           }
       db.close();
@@ -48,7 +53,7 @@ router.get('/list', function (req, res) {
 
 // Update one document
 router.post('/update', function (req, res) {
-  console.log('--USER_UPDATE--')
+  myLogModule.info('UserController API-UserList(user/update)')
   MongoClient.connect(url, function(err, db) {
     var payload = {
       "name": req.body.name
@@ -59,14 +64,11 @@ router.post('/update', function (req, res) {
   var newvalues = { $set: {name: "YO Rajesh" }}
   dbo.collection("customers").updateOne(myquery, newvalues, function(err, result) {
     if (err) throw err;
-    console.log("1 document updated");
+    myLogModule.info('User update sucessfully')
     res.send("1 document updated")
     db.close();
   });
   });
 })
 
-
-
- 
 module.exports = router
