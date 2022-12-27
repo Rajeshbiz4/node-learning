ab81619abf3b215d8155ef268cc49e7782ccfce8
Rajesh Pandhare <rajeshpandhare181@gmail.com>
2022-12-26 19:15:07 -0500

    image name changes

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,32 +1,27 @@
 const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage").GridFsStorage;
-var MongoClient = require('mongoose');
-var crypto = require('crypto');
-var path = require('path');
-var url = 'mongodb://127.0.0.1:27017/mydb';
+// var MongoClient = require('mongoose');
+// var url = 'mongodb://127.0.0.1:27017/mydb';
 // var url = `${process.env.MONGODB}/mydb`;
-const promise = MongoClient.connect(url, { useNewUrlParser: true });
+// const promise = MongoClient.connect(url, { useNewUrlParser: true });
 
-//create storage object
-const storage = new GridFsStorage({
-  db: promise,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        console.log("filename", filename);
-        console.log("fileInfo", fileInfo);
-        resolve(fileInfo);
-      });
-    });
+// upload image to folder
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+      let result = "";
+      const alphaNumericCharacters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const alphabetsLength = alphaNumericCharacters.length;
+      for (let i = 0; i < 8; i++) {
+        result += alphaNumericCharacters.charAt(Math.floor(Math.random() * alphabetsLength));
+      }
+      const randomNumber = Math.floor(Math.random() * 1000000);
+    
+    cb(null, randomNumber + "_" + result + "-" + file.originalname )
   }
-});
-// const upload = multer({ storage });
-module.exports = multer({ storage });
+})
+
+module.exports = multer({ storage: storage })
+// var upload = 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var port = 8000;
 var app = express();
-var multer  = require('multer')
+// var multer  = require('multer')
 const dotenv = require('dotenv');
 dotenv.config();
 // var router = express.Router();
@@ -14,6 +14,7 @@ var image_upload = require('./modules/imageUpload/imageUploadController.js');
 var complaint_controller = require('./modules/complaint/complaintController.js');
 var gramBody_controller = require('./modules/gram-body/gram-bodyController.js');
 var auth_Controller = require('./modules/auth/authController.js');
+var upload = require('./middleware/upload.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -22,27 +23,15 @@ app.use(bodyParser.json());
 app.use('/uploads',express.static('uploads'))
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application.     - developed by Rajesh Pandhare" });
+  res.json({ message: "Welcome to bezkoder application." });
 });
 app.use('/login', auth_Controller);
-app.use('/user', authenticateToken, user_controller);
+app.use('/user', user_controller);
 app.use('/notification',authenticateToken, notification_controller);
 app.use('/my-village', authenticateToken, myVillage_controller);
 app.use("/file", authenticateToken, image_upload);
 app.use("/complaint", authenticateToken, complaint_controller);
 app.use("/gram-body", authenticateToken, gramBody_controller);
-
-
-// upload image to folder
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-var upload = multer({ storage: storage })
 
 app.post('/upload', upload.single('profile-file'), function (req, res, next) {
   const imgUrl = `http://34.217.126.91:8000/uploads/${req.file.filename}`;
@@ -51,5 +40,4 @@ app.post('/upload', upload.single('profile-file'), function (req, res, next) {
 
 app.listen(port, () => {
 	console.log("Server listening on port " + port);
-  console.log(process.env.NODE_ENV)
   });
